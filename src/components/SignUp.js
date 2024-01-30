7676bfcf8a8a5178a905ff9b1d7fa7710f8cba64
Jass <frontendjass@gmail.com>
2024-01-30 17:27:19 -0600

    Added responsive design for desktops.

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -19,30 +19,39 @@ function SignUp() {
             <section id='sign-up'>
                 {!isSubscribed ? (
                     <>
-                        <img src="/assets/images/illustration-sign-up-mobile.svg" alt="hero"/>
-                        <h1 className="dark-slate-gray">Stay Updated!</h1>
-                        <p  className="dark-slate-gray">Join 60,000+ product managers receiving monthly updates on:</p>
-                        <ul>
-                            <li  className="dark-slate-gray">Product discovery and building what matters</li>
-                            <li  className="dark-slate-gray">Measuring to ensure updates are a success</li>
-                            <li  className="dark-slate-gray">And much more!</li>
-                        </ul>
-                        <form id="sign-up-form" onSubmit={handleSubmit}>
-                            <label htmlFor='email' className="dark-slate-gray">Email address</label>
-                            <input onChange={handleEmailChange} value={email} type="email" id="email" name="email" placeholder="email@company.com" required/>
-                            <button type="submit">Subscribe to monthly newsletter</button>
-                        </form>
+                    <div className="sign-up-border">
+                        <div>
+                            <img src="/assets/images/illustration-sign-up-mobile.svg" alt="hero" className="hero-image"/>
+                        </div>
+                            <div className="text-content">
+                                <h1 className="dark-slate-gray">Stay Updated!</h1>
+                                <p  className="dark-slate-gray">Join 60,000+ product managers receiving monthly updates on:</p>
+                                <ul>
+                                    <li  className="dark-slate-gray">Product discovery and building what matters</li>
+                                    <li  className="dark-slate-gray">Measuring to ensure updates are a success</li>
+                                    <li  className="dark-slate-gray">And much more!</li>
+                                </ul>
+                                <form id="sign-up-form" onSubmit={handleSubmit}>
+                                    <label htmlFor='email' className="dark-slate-gray">Email address</label>
+                                    <input onChange={handleEmailChange} value={email} type="email" id="email" name="email" placeholder="email@company.com" required/>
+                                    <button type="submit">Subscribe to monthly newsletter</button>
+                                </form>
+                        </div>
+                    </div>
                     </>
                 ) : (
                     <div className="subscribed-wrapper">
-                        <div>
+                        <div className="success-border">
                             <img src="/assets/images/icon-success.svg"/>
                             <h1>Thanks for subscribing!</h1>
                             <p>A confirmation email has been sent to <span className="dark-slate-gray bold">{email}</span>. 
                             Please open it and click the button inside to confirm your subscription.</p>
-                            
+
+
+                            <div><button onClick={() => setIsSubscribed(false)} className="button-dismiss">Dismiss message</button></div>
                         </div>
-                        <button onClick={() => setIsSubscribed(false)}>Dismiss message</button>
+                        
+                        
                     </div>
                 )}
             </section>
